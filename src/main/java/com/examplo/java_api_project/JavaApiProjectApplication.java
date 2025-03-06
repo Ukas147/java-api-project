e495b72c2317462fc1b9e59ddde4def48901dd01
Ukas147 <lucas.mshr@gmail.com>
2025-03-05 23:11:20 -0300

    feat: first commit

---
+++ src/main/java/com/examplo/java_api_project/JavaApiProjectApplication.java
@@ -0,0 +1,13 @@
+package com.examplo.java_api_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaApiProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaApiProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/examplo/java_api_project/JavaApiProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.examplo.java_api_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaApiProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
